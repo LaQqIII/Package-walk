2b5c4e8920bd7d459a28e4457859b0240f7280cd
Pushkov Vladislav Evgenievich <vepushkov@skr.local>
2021-09-17 15:35:44 +0300

    Опа давай давай 3

--- app/src/main/java/com/example/packagewalk/ui/PackageWalkNavGraph.kt
+++ app/src/main/java/com/example/packagewalk/ui/PackageWalkNavGraph.kt
@@ -2,13 +2,18 @@ package com.example.packagewalk.ui
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.packagewalk.ui.screens.authorization.Authorization
 import com.example.packagewalk.ui.screens.profile.Profile
 
 @Composable
 fun PackageWalkNavGraph(navController: NavHostController) {
+
+    val actions = remember(navController) { MainActions(navController) }
+
     NavHost(
         navController = navController,
         startDestination = Screens.PROGRESS.route
@@ -23,7 +28,16 @@ fun PackageWalkNavGraph(navController: NavHostController) {
             Text(text = "3")
         }
         composable(Screens.PROFILE.route) {
-            Profile()
+            Profile(navigateToScreenAuthorization = actions.navigateToScreenAuthorization)
         }
+        composable("authorization") {
+            Authorization()
+        }
+    }
+}
+
+class MainActions(navController: NavHostController) {
+    val navigateToScreenAuthorization: () -> Unit = {
+        navController.navigate("authorization")
     }
 }
--- app/src/main/java/com/example/packagewalk/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/packagewalk/ui/screens/profile/ProfileScreen.kt
@@ -1,10 +1,15 @@
 package com.example.packagewalk.ui.screens.profile
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
-@Preview(showBackground = true)
 @Composable
-fun Profile() {
+fun Profile(navigateToScreenAuthorization: () -> Unit) {
+
+    // todo добавить проверку залогинен пользователь или нет
+    if (true) {
+
+    } else {
+        navigateToScreenAuthorization()
+    }
 
 }
